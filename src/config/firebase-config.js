b89a4e1cd8eb62433c44546ad5a146d929551489
Maxime <maxime.lecieuxlmf@gmail.com>
2024-06-08 21:05:15 +0200

    Initialisation et configuration de firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxvyEsoSTvMMHuVgb20s8svZzaekIxKG4",
+  authDomain: "codersmonkeysformation.firebaseapp.com",
+  projectId: "codersmonkeysformation",
+  storageBucket: "codersmonkeysformation.appspot.com",
+  messagingSenderId: "567152743448",
+  appId: "1:567152743448:web:37ed3a74706bfcc9311f6d"
+};
+
+const app = initializeApp(firebaseConfig);
