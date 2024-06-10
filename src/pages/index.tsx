b58690e2cad7a18f80dbf0d8f72c920c63a040b3
Maxime <maxime.lecieuxlmf@gmail.com>
2024-06-10 19:54:00 +0200

    Création du composant de typographie + Ajout de clsx + Ajout du style Inter css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { Seo } from '@/ui/components/Seo'
+import { Typography } from '@/ui/design-system/typography/typography'
 
 export default function Home() {
   return (
     <>
       <Seo title="Coders Monkeys" description="Description"/>
-      <h1>Hello World !</h1>
+      <Typography variant="h1" component="h1">Coders Monkey</Typography>
     </>
   )
 }
